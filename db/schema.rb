b5b8c2c2f672da657506a5f0bf42e5038e0c596a
Tommaso Caporuscio <tommasocaporuscio@gmail.com>
2012-06-21 16:40:43 +0200

    assets precompile

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(:version => 20120619133005) do
     t.date     "data"
     t.string   "indirizzo"
     t.string   "citta"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
     t.string   "allegato_test_book_uid"
     t.string   "allegato_test_book_name"
     t.string   "email"
